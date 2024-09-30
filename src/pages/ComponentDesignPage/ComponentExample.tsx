190ccacaad5effdd3d3783a5c1ffb2239094201f
xianlang <1553093290@qq.com>
2024-09-30 16:08:25 +0800

    feat: 增加editorjs

--- src/pages/ComponentDesignPage/ComponentExample.tsx
+++ src/pages/ComponentDesignPage/ComponentExample.tsx
@@ -1,12 +1,20 @@
 import InputWithTag from './InputWithTag'
+import EditorJS from '@editorjs/editorjs'
 
 const ComponentExample = () => {
+  const editor = new EditorJS({
+    holder: 'editorjs',
+    hideToolbar: true,
+    defaultBlock: ''
+  })
+  console.log(editor)
   return (
     <div className='page-container'>
       <div style={{ width: '500px', display: 'flex', alignItems: 'center' }}>
         <p style={{ marginRight: '20px' }}>输入框输入回车添加tag</p>
         <InputWithTag />
       </div>
+      <div id='editorjs'></div>
     </div>
   )
 }
