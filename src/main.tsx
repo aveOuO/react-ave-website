1be712c6fa9ef5ac5278d251d74ab0bac4cd9e16
aveshh <1553093290@qq.com>
2024-11-08 15:51:57 +0800

    feat: 增加svg组件

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { ConfigProvider } from 'antd'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.ts'
+import 'virtual:svg-icons-register'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode></StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]'
+    })
+  ],
   // 模块解析
   resolve: {
     alias: [
