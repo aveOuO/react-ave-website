07dd0e537b8a0bfaea91d3e41b3270a708f05ff2
aveshh <1553093290@qq.com>
2025-01-03 20:18:48 +0800

    feat: 优化移动端布局

--- src/pages/CanvasPage/CanvasMaze.tsx
+++ src/pages/CanvasPage/CanvasMaze.tsx
@@ -478,9 +478,9 @@ export const CanvasMaze: React.FC = () => {
             onChange={handleGridSizeChange}
             disabled={isExploring}
             options={[
-              { value: 5, label: '5 x 5' },
-              { value: 10, label: '10 x 10' },
-              { value: 15, label: '15 x 15' },
+              // { value: 5, label: '5 x 5' },
+              // { value: 10, label: '10 x 10' },
+              // { value: 15, label: '15 x 15' },
               { value: 20, label: '20 x 20' },
               { value: 25, label: '25 x 25' },
               { value: 30, label: '30 x 30' },
